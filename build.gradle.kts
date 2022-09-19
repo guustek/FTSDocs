0700ec6ffd46c7633bdf98891ba7353f6bc2e348
Paweł Augustyniak <guustek@wp.pl>
2022-09-19 20:01:20 +0200

    Add Solr dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+
+    implementation("org.apache.solr:solr-solrj:9.0.0")
+    implementation("org.apache.solr:solr-core:9.0.0")
 }
 
 tasks.getByName<Test>("test") {
