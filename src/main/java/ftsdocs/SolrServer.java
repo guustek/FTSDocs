85ab8336393569ec959ce3509be58de2edfe5b94
Paweł Augustyniak <guustek@wp.pl>
2022-12-07 04:48:59 +0100

    Fix table columns, now only column with Path resizes, add TextFlow to display found content

--- src/main/java/ftsdocs/SolrServer.java
+++ src/main/java/ftsdocs/SolrServer.java
@@ -2,10 +2,7 @@ package ftsdocs;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Instant;
-import java.util.Date;
 
-import ftsdocs.model.Document;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
@@ -28,10 +25,10 @@ public class SolrServer {
                     SolrXmlConfig.fromSolrHome(resourceSolr.toPath(), System.getProperties()));
             this.coreContainer.load();
             this.server = new EmbeddedSolrServer(this.coreContainer, CORE_NAME);
-            this.server.deleteByQuery("*:*");
-            this.server.addBean(new Document("", "", Long.MAX_VALUE, "", Date.from(Instant.now()),
-                    Date.from(Instant.now())));
-            this.server.commit();
+//            this.server.deleteByQuery("*:*");
+//            this.server.addBean(new Document("", "", Long.MAX_VALUE, "", new Date(Long.MAX_VALUE),
+//                    new Date(Long.MIN_VALUE)));
+//            this.server.commit();
             log.info("Solr server initialized");
         } catch (Exception e) {
             log.error("Error while initializing Solr server", e);
--- src/main/java/ftsdocs/controller/MainController.java
+++ src/main/java/ftsdocs/controller/MainController.java
@@ -1,9 +1,9 @@
 package ftsdocs.controller;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -11,6 +11,7 @@ import ftsdocs.DisplayUtils;
 import ftsdocs.SolrService;
 import ftsdocs.model.Document;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -19,12 +20,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FileUtils;
-import org.apache.solr.client.solrj.SolrServerException;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -42,7 +46,9 @@ public class MainController implements Initializable {
     @FXML
     private Button searchButton;
     @FXML
-    private Button indexButton;
+    private Button indexFilesButton;
+    @FXML
+    private Button indexDirectoriesButton;
 
     @FXML
     private TableView<Document> documentTable;
@@ -53,33 +59,55 @@ public class MainController implements Initializable {
     @FXML
     private TableColumn<Document, String> documentCreationTimeColumn;
     @FXML
-    private TableColumn<Document, String> documentLastModificationTime;
+    private TableColumn<Document, String> documentModificationTimeColumn;
 
+    @FXML
+    private TextFlow textFlow;
     //endregion
 
     private final ObservableList<Document> documents = FXCollections.observableArrayList();
 
     private final SolrService solrService;
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         documentTable.setItems(documents);
         documentTable.setEditable(false);
-        documentNameColumn.setCellValueFactory(c -> new ReadOnlyStringWrapper(c.getValue().path()));
+        documentTable.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                String content = documentTable.getSelectionModel().getSelectedItem().getContent();
+                Text text = new Text(content);
+                text.wrappingWidthProperty().bind(textFlow.widthProperty().subtract(10));
+                textFlow.getChildren().clear();
+                textFlow.getChildren().add(text);
+            }
+        });
+        documentNameColumn.prefWidthProperty().bind(getPathColumnSize());
+        documentNameColumn.setCellValueFactory(
+                c -> new ReadOnlyStringWrapper(c.getValue().getPath()));
         documentSizeColumn.setCellValueFactory(c ->
                 new ReadOnlyStringWrapper(
-                        FileUtils.byteCountToDisplaySize(c.getValue().fileSize())));
+                        FileUtils.byteCountToDisplaySize(c.getValue().getFileSize())));
         documentCreationTimeColumn.setCellValueFactory(c ->
                 new ReadOnlyStringWrapper(
-                        DisplayUtils.dateTimeFormatter.format(c.getValue().creationTime())));
-        documentLastModificationTime.setCellValueFactory(c ->
+                        DisplayUtils.dateTimeFormatter.format(
+                                c.getValue().getCreationTime().toInstant())));
+        documentModificationTimeColumn.setCellValueFactory(c ->
                 new ReadOnlyStringWrapper(
-                        DisplayUtils.dateTimeFormatter.format(c.getValue().lastModifiedTime())));
+                        DisplayUtils.dateTimeFormatter.format(
+                                c.getValue().getLastModifiedTime().toInstant())));
+    }
+
+    private ObservableValue<? extends Number> getPathColumnSize() {
+        return documentTable.widthProperty()
+                .subtract(documentSizeColumn.widthProperty())
+                .subtract(documentCreationTimeColumn.widthProperty())
+                .subtract(documentModificationTimeColumn.widthProperty());
     }
 
     @FXML
-    private void searchButtonClicked(MouseEvent mouseEvent)
-            throws SolrServerException, IOException {
+    private void searchButtonClicked(MouseEvent mouseEvent) {
         String query = searchTextField.getText();
         if (query.isBlank()) {
             query = "*";
@@ -89,13 +117,21 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    private void indexButtonClicked(MouseEvent mouseEvent) throws IOException, SolrServerException {
-        FileChooser fileChooser = new FileChooser();
-        List<File> files = fileChooser
+    private void indexFilesButtonClicked(MouseEvent mouseEvent) {
+        FileChooser chooser = new FileChooser();
+        List<File> files = chooser
                 .showOpenMultipleDialog(root.getScene().getWindow());
         if (files != null) {
             solrService.indexFiles(files);
         }
+    }
 
+    @FXML
+    private void indexDirectoriesButtonClicked(MouseEvent mouseEvent) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        File directory = chooser.showDialog(root.getScene().getWindow());
+        if (directory != null) {
+            solrService.indexFiles(Collections.singletonList(directory));
+        }
     }
 }
